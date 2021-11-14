36a1283ec314c7decb89aa1da48fbd30aa737be6
Robert Zhang <jiaxuanzhang066@gmail.com>
2021-11-14 13:16:00 -0500

    initial commit

---
+++ src/app.ml
@@ -0,0 +1,19 @@
+open Core
+open Lwt
+open Cohttp
+open Cohttp_lwt_unix
+
+let body =
+  Client.get
+    (Uri.of_string "https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=5m")
+  >>= fun (res, body) ->
+  let code = res |> Response.status |> Code.code_of_status in
+  Printf.printf "Response code: %d\n" code;
+  Printf.printf "Headers: %s\n" (res |> Response.headers |> Header.to_string);
+  body |> Cohttp_lwt.Body.to_string >|= fun body -> body
+;;
+
+let () =
+  let body = Lwt_main.run body in
+  print_endline ("Received body\n" ^ body)
+;;
---
+++ src/lib.mli
@@ -0,0 +1,3 @@
+(* 
+  This is file contains specifications to our project
+*)
